22bc775f518ec9bc6ce2cf23bcaaa2c8e43c8524
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-10-01 13:15:37 +0200

    Fix broken pad zero test

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ describe('generate', () => {
     for (let i = 0, l = 3; i < l; i++) {
       const pin = generate(today);
       expect(+pin.slice(0, 4)).toEqual(y);
-      expect(pin.slice(4, 6)).toEqual(padZero(m));
+      expect(pin.slice(4, 6)).toEqual('' + padZero(m));
       expect(pin.slice(6, 8)).toEqual('' + padZero(d));
       expect(personnummer.valid(pin)).toEqual(true);
     }
